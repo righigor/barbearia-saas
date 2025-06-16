cf7cd5ad705b5bae5cea9e0b14dbbc21a5691cd7
igor <cruigor@gmail.com>
2025-06-16 15:46:03 -0300

    feat: add booking item

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Header from "@/_components/header"
+import { Avatar, AvatarImage } from "@/_components/ui/avatar"
+import { Badge } from "@/_components/ui/badge"
 import { Button } from "@/_components/ui/button"
+import { Card, CardContent } from "@/_components/ui/card"
 import { Input } from "@/_components/ui/input"
 import { SearchIcon } from "lucide-react"
 import Image from "next/image"
@@ -27,6 +30,28 @@ export default function Home() {
             alt="Banner"
           />
         </div>
+
+        {/* agendamento */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confimado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">Junho</p>
+              <p className="text-2xl font-bold">06</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
